41db65c9e630acecfa5e0942e814a7b5a740503c
Deependra <deependras1110@gmail.com>
2022-09-24 01:29:11 -0500

    App-top and app-bottom

---
+++ car-quest/src/components/bottom/Bottom.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './bottom.css'
+
+function Bottom() {
+  return (
+    <div className='bottom-container'>
+        <div className='status-bar'>
+            
+        </div> 
+    </div>
+  )
+}
+
+export default Bottom
